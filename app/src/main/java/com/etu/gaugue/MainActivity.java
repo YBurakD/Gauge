3bd143b5fec2f9b759db4ca57f9763182916b7c1
YBurakD <y.burak.durmus@gmail.com>
2026-05-01 15:36:07 +0300

    initializations

--- app/src/main/java/com/etu/gaugue/MainActivity.java
+++ app/src/main/java/com/etu/gaugue/MainActivity.java
@@ -1,6 +1,13 @@
 package com.etu.gaugue;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // i need a sensor manager to get sensor data
+    SensorManager sensorManager;
+    Sensor linearAccelSensor;
+    Sensor gravitySensor;
+
+    // gravity values, starting with earth gravity
+    float[] gravity = {0f, 9.8f, 0f};
+
+    // velocity starts at zero
+    float velocity = 0f;
+    long lastTimestamp = 0;
+    float stopSign = 0f;
+    int graceFrames = 0;
+
+    // the gauge view and controls
+    GaugeView gaugeView;
+    SeekBar seekBar;
+    CheckBox checkboxManual;
+    boolean manualMode = false;
+
+    // alarm stuff
+    ToneGenerator toneGen;
+    boolean alarmSounding = false;
+    Handler alarmHandler = new Handler(Looper.getMainLooper());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
